2f06f7da88dd7af857e92d85f135a15f0646da43
Gaya <normalg213@gmail.com>
2023-08-10 11:52:38 +0100

    Graph

--- src/views/HourlyWeatherView.js
+++ src/views/HourlyWeatherView.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import styles from "../assets/values/HourlyWeatherView.module.css";
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, LabelList, ResponsiveContainer } from 'recharts';
+import styles from '../assets/values/HourlyWeatherView.module.css';
 
 const WeatherDayView = ({ HourlylyWeather, day }) => {
-  // Get the hourly weather data for the selected day
   const selectedDayWeather = HourlylyWeather[day];
 
+  const chartData = selectedDayWeather.hourlyWeatherArray.map(hourData => ({
+    hour: `${hourData.hour}:00`,
+    temperature: hourData.temperature,
+  }));
+
+  const minTemperature = Math.min(...chartData.map(dataPoint => dataPoint.temperature));
+  const maxTemperature = Math.max(...chartData.map(dataPoint => dataPoint.temperature));
+  const yAxisPadding = 1;
+  const xAxisPadding = 10;
+
+  // Ajuster la hauteur en définissant le domaine de l'axe Y
+  const yDomain = [minTemperature, maxTemperature + yAxisPadding];
+  const yRange = yDomain[1] - yDomain[0];
+  const adjustedYDomain = [minTemperature - 5, maxTemperature + yAxisPadding + yRange * 0.2]; // Réduire la hauteur de 20%
+
   return (
-    <div>
-      <div className={styles["hourly-weather"]}>
-        {selectedDayWeather.hourlyWeatherArray.map(hourData => (
-          <div key={hourData.hour} className={styles["hour"]}>
-            <p>{hourData.hour}:00</p>
-            <p><img src={hourData.weatherLogo} alt="Weather Logo" className={styles.weatherLogo} /></p>
-            <p>{hourData.temperature}°C</p>
-          </div>
-        ))}
-      </div>
+    <div className={styles['hourly-weather']}>
+      <ResponsiveContainer width="100%" height={200} className={styles['chart-container']}>
+        <LineChart data={chartData} margin={{ left: xAxisPadding, right: xAxisPadding }}>
+          <XAxis dataKey="hour" interval={2} />
+          <YAxis hide domain={adjustedYDomain} />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="temperature" stroke="#8884d8" dot={{ r: 6 }} />
+          <LabelList dataKey="temperature" position="top" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
