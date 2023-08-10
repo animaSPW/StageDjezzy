2e3b73fbfbec536d116f62a0303500ef24fffa43
maison <hammoudimohamedanis@gmail.com>
2023-08-10 13:23:02 +0100

    added

--- src/assets/values/constants.js
+++ src/assets/values/constants.js
@@ -1,4 +1,4 @@
 export const ApiKey = '5c1b0eda86a5440bb8d110703230208'
-export const baseUrlF = ' http://api.weatherapi.com/v1/forecast.json'
+export const baseUrlF = ' https://api.weatherapi.com/v1/forecast.json'
 export const baseUrlP = "https://pokeapi.co/api/v2/type/";
 export const baseUrlP2 = "https://pokeapi.co/api/v2/pokemon/";
