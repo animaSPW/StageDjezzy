ebb62d344ca5e56cacb257458767f3d67305aadf
Gaya <normalg213@gmail.com>
2023-08-10 13:34:36 +0100

    lolo

--- src/views/HourlyWeatherView.js
+++ src/views/HourlyWeatherView.js
@@ -8,6 +8,7 @@ const WeatherDayView = ({ HourlylyWeather, day }) => {
   const chartData = selectedDayWeather.hourlyWeatherArray.map(hourData => ({
     hour: `${hourData.hour}:00`,
     temperature: hourData.temperature,
+    weatherLogo: hourData.weatherLogo,
   }));
 
   const minTemperature = Math.min(...chartData.map(dataPoint => dataPoint.temperature));
@@ -15,18 +16,29 @@ const WeatherDayView = ({ HourlylyWeather, day }) => {
   const yAxisPadding = 1;
   const xAxisPadding = 14;
 
-  // Ajuster la hauteur en définissant le domaine de l'axe Y
   const yDomain = [minTemperature, maxTemperature + yAxisPadding];
   const yRange = yDomain[1] - yDomain[0];
-  const adjustedYDomain = [minTemperature - 5, maxTemperature + yAxisPadding + yRange * 0.2]; // Réduire la hauteur de 20%
+
+  const CustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      const data = payload[0].payload;
+      return (
+        <div className={styles['custom-tooltip']}>
+          <img src={data.weatherLogo} alt="Weather Logo" className={styles.weatherLogo} />
+          <p>{`${data.temperature}°C`}</p>
+        </div>
+      );
+    }
+    return null;
+  };
 
   return (
     <div className={styles['hourly-weather']}>
       <ResponsiveContainer width="100%" height={200} className={styles['chart-container']}>
         <LineChart data={chartData} margin={{ left: xAxisPadding, right: xAxisPadding }}>
           <XAxis dataKey="hour" interval={2} />
-          <YAxis hide domain={adjustedYDomain} />
-          <Tooltip />
+          <YAxis hide domain={[minTemperature - 5, maxTemperature + yAxisPadding + yRange * 0.2]} />
+          <Tooltip content={<CustomTooltip />} />
           <Legend />
           <Line type="monotone" dataKey="temperature" stroke="#8884d8" dot={{ r: 6 }} />
           <LabelList dataKey="temperature" position="top" />
